58797891fc5d3fbeadb94d16866d37a687fb16d8
D. Taylor <senseitg@gmail.com>
2016-03-08 15:44:34 +0100

    performance+++

--- sketches/test3/test3.ino
+++ sketches/test3/test3.ino
@@ -36,44 +36,47 @@ int16_t fp_atn2(int16_t y, int16_t x) {
 uint8_t i_sqrt(uint16_t val) {
   uint8_t root = 0, temp = 32;
   while(temp) {
-    if(((uint16_t)(root + temp) * (root + temp)) <= val) root += temp;
+    if(((root + temp) * (root + temp)) <= val) root += temp;
     temp = temp >> 1;
   }
   return root; 
 }
 
 // Indexer for motion
-int ix;
-
+byte ix;
 void loop() {
+#ifdef __MINGW32__
   delay(40);
-  ix++;
-  
-  arduboy.clearDisplay();
-  for (int x = 0; x < 128; x++) {
-    for (int y = 0; y < 64; y++) {
+#endif
+  ix++; // TODO: correction on roll over 255 => ? (where ? is currently 0)
+  for(byte yz = 0; yz < 64; yz += 8) {
+    for (byte x = 0; x < 128; x++) {
       int xs = x - 64;
-      int ys = y - 32;
-      // avoid divide by zero
-      if(xs == 0 && ys == 0) continue;
-
-      // sqrt needs to be optimized, possibly also division if possible
-      int dst = SCALE_Y / i_sqrt(xs * xs + ys * ys);
-      if(dst > 60) continue; // No point in rendering the aliased centre
-      if(dst > 25 && (1&(xs^ys))) continue; // Shadow far region
-      int tex_y = (dst + ix) >> 2;
-
-      // can be further optimized
-      int tex_x = ((fp_atn2(ys, xs) + ix) * TEX_W * SCALE_X) / 1602;
-      
-      // transfer pixel from texture
-      //bool pixel = readtex(tex_x, tex_y); // TEXTURE
-      bool pixel = 1&(tex_x^tex_y);     // CHECKERPATTERN
-      
-      if(pixel) arduboy.drawPixel(x, y, 1);
+      byte cell = 0;bool pixel;
+      for(byte y = yz; y < yz + 8; y++) {
+        int ys = y - 32;
+        // Mask out centre + avoid divide by zero
+        if(xs < -10 || xs > 10 || ys < -10 || ys > 10) {
+          // Calculate distance from centre, perspective correction costs one division
+          byte dst = SCALE_Y / i_sqrt(xs * xs + ys * ys);
+          // Mask out centre and apply shadow on far region
+          if(dst <= 60 && (dst < 25 || (1 & (xs ^ ys)))) {
+            // Animate Y
+            byte tex_y = (dst + ix) >> 2;
+            // Animate X, calculate angle
+            byte tex_x = ((fp_atn2(ys, xs) + ix) * TEX_W * SCALE_X) / 1602;
+            // Calculate pixel color
+            //pixel = readtex(tex_x, tex_y); // USE TEXTURE
+            pixel = 1 & (tex_x ^ tex_y);   // USE CHECKERS
+            cell |= pixel << 7;
+          }
+        }
+        if(7 == (y & 7)) {
+          SPI.transfer(cell);
+          cell = 0;
+        } else cell >>= 1;
+      }
     }
   }
-  arduboy.display();
-  
 }
 /* ========================================================== */
