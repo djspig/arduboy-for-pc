ebf4325579e2e6f4a11e8f2556ff14c93d93ff15
D. Taylor <senseitg@gmail.com>
2016-03-11 14:18:48 +0100

    code size reduction

--- sketches/test4/test4.ino
+++ sketches/test4/test4.ino
@@ -2,28 +2,30 @@
 #include "resource.h"
 
 static Arduboy arduboy;
-static SimpleButtons buttons (arduboy);
+static SimpleButtons buttons(arduboy);
 
 static byte *video;
 
 // Ship parameters
-#define SHIP_DEC 40    // Decelleration
-#define SHIP_ACC 20    // Accelleration
-#define SHIP_SPD 2000  // Top speed
+#define SHIP_DEC   40 // Decelleration
+#define SHIP_ACC   20 // Accelleration
+#define SHIP_SPD 2000 // Top speed
 
-// Tunnel texture (this could be much better)
-#define TEXTURE(A, B) (((A) ^ (B)) & 0x10) // CHECKERS
+// Tunnel texture (this needs work for sure)
+#define TEXTURE(A, B, I) ((((A)+(I)) ^ ((B)+(I))) & 0x10) // Checkerboard pattern
 
-// Varipius helper macros
-#define INPUT(X) (input & (X))
-#define ISGN(X) ((X) == 0 ? 0 : (X) > 0 ? -1 : +1)
+// Various helper macros
+#define INPUT(X) (input & (X))                      // Check for button pressed
+#define ISGN(X)  ((X) == 0 ? 0 : (X) > 0 ? -1 : +1) // Inverse sign function
+#define MSB(X)   ((X) >> 0x8)                       // Extract most significant byte
+#define LSB(X)   ((byte)(X))                        // Extract least significant byte
 
 // Local variables
 static int  vx, vy; // Ship velocity
 static int  sx, sy; // Ship coordinates
 static int  bx, by; // Bullet coordinates
 static byte bl;     // Bullet lifetime
-static byte twist;  // Tunnel twist multiplier
+static byte twist;  // Tunnel twist multiplier (TAKEN OUT)
 static word ix;     // Animation indexer
 static int  hx, hy; // Autotracking tunnel centre coordinates
 
@@ -43,13 +45,8 @@ void loop() {
   // Poll for user input
   byte input = buttons.poll();
 
-  // Progress animation
+  // Progress animations
   ix += 2;
-
-  // Compute ship position (replace with user control?)
-  //sx = sin((float)ix / 113) * 64;
-  //sy = cos((float)ix / 67) * 32;
-  //twist = sin((float)ix / 283) * 64; // Se below
 
   // User control
   bool engine = false;
@@ -61,15 +58,14 @@ void loop() {
   if(INPUT(BTN_B) && !bl) {
     bl = 64; bx = sx; by = sy;
     twist = 120;
-  } else if(twist) twist = (twist * 250) >> 8;
-  
-  bx = (bx * 250) >> 8;
-  by = (by * 250) >> 8;
-  
+  } else if(twist) twist = MSB(twist * 250);
+
+  bx = MSB(bx * 250);
+  by = MSB(by * 250);
+
   if(vx > 0) vx = max(0, vx - SHIP_DEC); else if(vx < 0) vx = min(0, vx + SHIP_DEC);
   if(vy > 0) vy = max(0, vy - SHIP_DEC); else if(vy < 0) vy = min(0, vy + SHIP_DEC);
-  
-  
+
   // Control ship position
   sx += vx; sy += vy;
   if(sx > +30000) { vx = -vx; sx = +60000 - sx; }
@@ -78,8 +74,8 @@ void loop() {
   if(sy < -15000) { vy = -vy; sy = -30000 - sy; }
   
   // Compute high(er) resolution, low passed ship position
-  hx += ((sx >> 8) * 8 - hx) / 16;
-  hy += (((sy >> 8) + 6) * 16 - hy) / 8;
+  hx += ((MSB(sx)) * 8 - hx) / 16;
+  hy += ((MSB(sy) + 6) * 16 - hy) / 8;
   
   // Convert to tunnel center coordinates
   byte cx = (-hx / 16) + 64, cy = (-hy / 32) + 32;
@@ -94,20 +90,12 @@ void loop() {
     byte *vptr = &video[dx];
     for(dy = 0; dy < cy; dy++, yterm--, b8 >>= 1) {
       word t = pgm_read_word(&lut[128 * yterm + xterm]); // Quadrant 1
-      if((byte)t != 0) {                                 // Shadow mask
-        byte tx = ((64 + (t >> 8)) + ix);                // Animate X
-        word ty = ((t & 0xFF) + ix);                     // Animate Y
-        if(TEXTURE(tx, ty)) b8 |= 0x80;
-      }
+      if(LSB(t) != 0 && TEXTURE(64 + MSB(t), LSB(t), ix)) b8 |= 0x80;
       if(7 == (dy & 7)) *vptr = b8, vptr += 128;
     }
     for(; dy < 64; dy++, yterm++, b8 >>= 1) {
       word t = pgm_read_word(&lut[128 * yterm + xterm]); // Quadrant 2
-      if((byte)t != 0) {                                 // Shadow mask
-        byte tx = ((63 - (t >> 8)) + ix);                // Animate X
-        word ty = ((t & 0xFF) + ix);                     // Animate Y
-        if(TEXTURE(tx, ty)) b8 |= 0x80;
-      }
+      if(LSB(t) != 0 && TEXTURE(63 - MSB(t), LSB(t), ix)) b8 |= 0x80;
       if(7 == (dy & 7)) *vptr = b8, vptr += 128;
     }
   }
@@ -116,24 +104,17 @@ void loop() {
     byte *vptr = &video[dx];
     for(dy = b8 = 0; dy < cy; dy++, yterm--, b8 >>= 1) {
       word t = pgm_read_word(&lut[128 * yterm + xterm]); // Quadrant 3
-      if((byte)t != 0) {                                 // Shadow mask
-        byte tx = ((191 - (t >> 8)) + ix);               // Animate X
-        word ty = ((t & 0xFF) + ix);                     // Animate Y
-        if(TEXTURE(tx, ty)) b8 |= 0x80;
-      }
+      if(LSB(t) != 0 && TEXTURE(191 - MSB(t), LSB(t), ix)) b8 |= 0x80;
       if(7 == (dy & 7)) *vptr = b8, vptr += 128;
     }
     for(; dy < 64; dy++, yterm++, b8 >>= 1) {
       word t = pgm_read_word(&lut[128 * yterm + xterm]); // Quadrant 4
-      if((byte)t != 0) {                                 // Shadow mask
-        byte tx = ((192 + (t >> 8))  + ix);              // Animate X
-        word ty = ((t & 0xFF) + ix);                     // Animate Y
-        if(TEXTURE(tx, ty)) b8 |= 0x80;
-      }
+      if(LSB(t) != 0 && TEXTURE(192 + MSB(t), LSB(t), ix)) b8 |= 0x80;
       if(7 == (dy & 7)) *vptr = b8, vptr += 128;
     }
   }
 
+  // Screen flashing
   if(twist > 110) {
     byte *vptr = video;
     while(vptr < &video[1024]) *vptr++ = 0xFF;
@@ -145,25 +126,25 @@ void loop() {
   // Coord-system conversion
   int wx, wy;
   
-  // Draw bullets
-  wx = (bx >> 8) + cx - 7;
-  wy = (by >> 8) + cy;
+  // Draw bombs
+  wx = MSB(bx) + cx - 7;
+  wy = MSB(by) + cy;
   if(bl) {
     bl--;
     arduboy.drawSprite(wx, wy, bullet, 7, 8, (bl >> 3) + (ix & 8 ? 1 : 0), BLACK);
     if(!(ix & 4)) arduboy.drawSprite(wx, wy, bullet, 7, 8, (bl >> 3) + (ix & 8 ? 9 : 1), WHITE);
   }
   
-  // Draw laser
-  wx = (sx >> 8) + cx;
-  wy = (sy >> 8) + cy + 2;
+  // Draw lasers
+  wx = MSB(sx) + cx;
+  wy = MSB(sy) + cy + 2;
   if(INPUT(BTN_A) && (ix & 2)) {
     arduboy.drawLine(wx + (ix & 4 ? -4 : +4), wy, cx + (ix & 4 ? -1 : +1), cy, WHITE);
   }
   
   // Draw ship
-  wx = (sx >> 8) + cx - 7;
-  wy = (sy >> 8) + cy;
+  wx = MSB(sx) + cx - 7;
+  wy = MSB(sy) + cy;
   arduboy.drawSprite(wx, wy, ship, 15, 16, 0, BLACK);
   arduboy.drawSprite(wx, wy, ship, 15, 16, 1, WHITE);
   if(engine && ix & 4) arduboy.drawSprite(wx, wy, ship, 15, 16, 2, WHITE); // Engine
